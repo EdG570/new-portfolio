47cc302a2721a518cc7c4270f9a3ea00cb9b8882
Ed Glazenski <edglazenski@gmail.com>
2016-03-15 00:21:24 -0400

    Added portfolio project detail content and animations. Changed home page header image.

--- app/js/main.js
+++ app/js/main.js
@@ -16,10 +16,208 @@ $(document).ready(function() {
     $(this).parent().siblings().hide();
     $(this).hide();
     $(this).siblings('.back').show();
-  });     
+  });  
 
-  $('#js').velocity({
-    
+  $('#yoda-detail').hide();
+  $('#snack-detail').hide(); 
+  $('#quiz-detail').hide();
+  $('#countries-detail').hide();
+  $('#band-detail').hide();  
+  $('#flick-detail').hide();
+  $('#wait-detail').hide();
+  $('#abg-detail').hide();
+  $('#flappy-detail').hide();
+  $('#portraits-detail').hide();
+  $('#grocery-detail').hide();
+  $('#hot-detail').hide();
+
+  $('#yoda-img').click(function() {
+    $('#yoda-detail').show();
+    $('#yoda-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#snack-img').click(function() {
+    $('#snack-detail').show();
+    $('#snack-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#quiz-img').click(function() {
+    $('#quiz-detail').show();
+    $('#quiz-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#countries-img').click(function() {
+    $('#countries-detail').show();
+    $('#countries-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#band-img').click(function() {
+    $('#band-detail').show();
+    $('#band-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#flick-img').click(function() {
+    $('#flick-detail').show();
+    $('#flick-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#wait-img').click(function() {
+    $('#wait-detail').show();
+    $('#wait-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#abg-img').click(function() {
+    $('#abg-detail').show();
+    $('#abg-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#flappy-img').click(function() {
+    $('#flappy-detail').show();
+    $('#flappy-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#portraits-img').click(function() {
+    $('#portraits-detail').show();
+    $('#portraits-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#grocery-img').click(function() {
+    $('#grocery-detail').show();
+    $('#grocery-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  $('#hot-img').click(function() {
+    $('#hot-detail').show();
+    $('#hot-detail').velocity({
+        height: '100%',
+        opacity: 1
+    }, 2000);
+    $('.detail-text').velocity({
+        opacity: 1
+    }, 2000);
+    $('.detail-img').velocity({
+        opacity: 1
+    }, 2000);
+  });
+
+  // Hides project detail container
+  $('.cancel').click(function() {
+    $(this).parent().velocity({
+      height: '0',
+      opacity: 0
+    }, 2000, function() {
+      $(this).hide();
+    });
+    $('.detail-text').velocity({
+      opacity: 0
+    }, 1000);
+    $('.detail-img').velocity({
+      opacity: 0
+    }, 1000);
+  });
+
+  // Scrolls to project detail container
+  $('.port-flex').find('img').click(function() {
+    $('#portfolio-section').find('h2').animatescroll();
   });
 
 });
